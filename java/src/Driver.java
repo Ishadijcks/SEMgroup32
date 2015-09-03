88c476a1efecd4472d7f16688a8680cb80458143
Isha <I.E.Dijcks@student.tudelft.nl>
2015-09-03 14:46:20 +0200

    Bubbles are now drawn in color with a black border

--- java/src/Driver.java
+++ java/src/Driver.java
@@ -1,5 +1,6 @@
 
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -21,13 +22,16 @@ public class Driver extends JPanel  {
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 		
-		// Draw all the bubbles
+		// Draw all the bubbles with a black border
 		Level curLevel = game.getLevelList().get(game.getCurrentLevel());
 		for(int i = 0; i<curLevel.getBubbleList().size(); i++){
 			Bubble bubble = curLevel.getBubbleList().get(i);
+			g2d.setColor(bubble.getColor());
 			g2d.fillOval(bubble.getX(), bubble.getY(), bubble.getRadius(), bubble.getRadius());
+			g2d.setColor(Color.black);
+			
+			g2d.drawOval(bubble.getX()-1,bubble.getY()-1,bubble.getRadius()+2,bubble.getRadius()+2);
 		}
-		
 		// Draw all the players
 		for(int i = 0; i<curLevel.getPlayerList().size(); i++){
 			Player player = curLevel.getPlayerList().get(i);
@@ -39,7 +43,7 @@ public class Driver extends JPanel  {
 		// Draw the ropes
 		if(curLevel.hasRope()){
 		g2d.drawLine(curLevel.getRope().getX(), curLevel.getRope().getY(), curLevel.getRope().getX(), curLevel.getHeight());
-		}
+		}	
 		// Draw the border
 		g2d.drawRect(1, 1, curLevel.getWidth(), curLevel.getHeight());
 
@@ -55,10 +59,10 @@ public class Driver extends JPanel  {
 		
 		Player isha = new Player("Isha",40,10);
 		Player tim = new Player("Tim",80,10);
-		Bubble bubble1 = new Bubble(10,50,50,true,false);
-		Bubble bubble2 = new Bubble(10,50,50,true,true);
-		Bubble bubble3 = new Bubble(10,50,50,false,false);
-		Bubble bubble4 = new Bubble(10,50,50,false,true);
+		Bubble bubble1 = new Bubble(4,50,50,true,false);
+		Bubble bubble2 = new Bubble(8,50,50,true,true);
+		Bubble bubble3 = new Bubble(16,50,50,false,false);
+		Bubble bubble4 = new Bubble(8,50,50,false,true);
 		Level level1 = new Level();
 		level1.addBubble(bubble1);
 		level1.addBubble(bubble2);
