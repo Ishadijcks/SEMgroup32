aacf6bdd897d8e1e9a937bb1a2a4db1878d80eaf
Naomi de Ridder <naomi-ah@hotmail.com>
2015-09-08 11:43:59 +0200

    started on startscreen test (nothing valuable yet)

--- java/src/junit/StartScreenTest.java
+++ java/src/junit/StartScreenTest.java
@@ -1,11 +1,22 @@
 package junit;
 import static org.junit.Assert.*;
 
+import javax.swing.JFrame;
+
 import org.junit.Test;
 
 
 public class StartScreenTest {
 
+	public Startscreen startscreen;
+	
+	@Before
+	public void init(){
+		
+	}
+	
+	startscreen = new StartScreen(Driver driver, JFrame jframe);
+	
 	@Test
 	public void testStartScreen() {
 		fail("Not yet implemented");
