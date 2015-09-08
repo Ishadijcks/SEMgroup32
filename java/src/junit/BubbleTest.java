0815eeec8f7df3b6f55ee1f35dbe08e01dc58c1f
Floris List <f.e.c.list@student.tudelft.nl>
2015-09-08 09:58:11 +0200

    Created test cases

---
+++ java/src/junit/BubbleTest.java
@@ -0,0 +1,49 @@
+package junit;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BubbleTest {
+
+	@Test
+	public void testBubble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBounceH() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBounceV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDiameter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+}
