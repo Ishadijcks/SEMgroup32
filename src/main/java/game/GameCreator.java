3e2c6e266af009127393572ba3f1b24bf449ff9f
Naomi de Ridder <naomi-ah@hotmail.com>
2015-09-11 22:09:45 +0200

    Javadoc in GameCreator and Powerup

--- src/main/java/game/GameCreator.java
+++ src/main/java/game/GameCreator.java
@@ -3,6 +3,11 @@ package game;
 
 public class GameCreator {
 
+	/**
+	 * Create a game with a single player. 
+	 * @param player1
+	 * @return a game with a single player
+	 */
     public static Game createSinglePlayer(Player player1) {
         Game game = new Game();
         game.addPlayer(player1);
@@ -13,6 +18,12 @@ public class GameCreator {
         return game;
     }
 
+    /**
+     * Create a game with multiple players
+     * @param player1
+     * @param player2
+     * @return a game with multiple players
+     */
     public static Game createMultiPlayer(Player player1, Player player2) {
         Game game = new Game();
         game.addPlayer(player1);
