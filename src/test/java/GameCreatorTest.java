b732606e3a67e6b9ca1827f5ed482cb010f67828
Naomi de Ridder <naomi-ah@hotmail.com>
2015-09-11 21:46:17 +0200

    create GameCreatorTest

---
+++ src/test/java/GameCreatorTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import game.Game;
+
+import org.junit.Test;
+
+
+public class GameCreatorTest {
+
+	 Game game = new Game();
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
