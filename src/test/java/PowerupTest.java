5bfa0ee9695b5b6c6e53b14ddf7f835cef0a4cfb
Naomi de Ridder <naomi-ah@hotmail.com>
2015-09-11 20:01:31 +0200

    Updated Poweruptest

--- src/test/java/PowerupTest.java
+++ src/test/java/PowerupTest.java
@@ -1,39 +1,36 @@
-
 import static org.junit.Assert.*;
 import game.Powerup;
+import game.Settings;
 
 import org.junit.Before;
 import org.junit.Test;
-
 
 public class PowerupTest {
 
 	public int x;
 	public int y;
 	public Powerup powerup;
-	
+	public int framesLeft;
+    int height = Settings.getPowerupHeight();
+    int width = Settings.getPowerupWidth();
+
 	@Before
 	public void init() {
 		x = 3;
-		y = 5;	
+		y = 5;
 		String name = "speed";
-			
-		powerup = new Powerup(name, x, y);		
+
+		powerup = new Powerup(name, x, y);
 	}
-	
+
 	@Test
-	public void testEquals(){
+	public void testEquals() {
 		Powerup powerup1 = new Powerup("name", 2, 3);
 		Powerup powerup2 = new Powerup("name", 2, 3);
 		Powerup powerup3 = new Powerup("name", 2, 4);
 		assertTrue(powerup1.equals(powerup2));
 		assertFalse(powerup1.equals(powerup3));
-		
-	}
-	
-	@Test
-	public void testPowerup() {
-		fail("Not yet implemented");
+
 	}
 
 	@Test
@@ -43,32 +40,27 @@ public class PowerupTest {
 
 	@Test
 	public void testDecreaseFramesLeft() {
-		fail("asd");
+			int framesLeft = 10;
+	powerup.decreaseFramesLeft();
+	assertEquals(framesLeft, 9);
 	}
-
+	
 	@Test
 	public void testIsActive() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetX() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetY() {
-		fail("Not yet implemented");
+		assertTrue(powerup.isActive());
 	}
 
 	@Test
 	public void testGetHeight() {
-		fail("Not yet implemented");
+		int height2 = 10;
+		assertEquals(height,height2);
+		
 	}
 
 	@Test
 	public void testGetWidth() {
-		fail("Not yet implemented");
+		int width2 = 15;
+		assertEquals(width, width2);
 	}
 
 	@Test
