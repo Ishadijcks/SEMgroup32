ea4d798cefa403056522706c92b296ca4fbc0ddb
tim <timbuckers@gmail.com>
2015-09-15 21:27:50 +0200

    Filter log added

--- src/main/java/game/LogObject.java
+++ src/main/java/game/LogObject.java
@@ -1,27 +1,49 @@
 package game;
 
+/**
+ * A simple log object.
+ * @author Tim
+ *
+ */
 public class LogObject {
     private String message;
     private int category;
 
     private int severity;
     
-    public LogObject(String message, int category2, int severity2){
+    /**
+     * The constructor for the log object.
+     * @param message
+     * @param categoryInput
+     * @param severityInput
+     */
+    public LogObject(String message, int categoryInput, int severityInput){
         this.message = message;
-        this.category = category2;
-        this.severity = severity2;
+        this.category = categoryInput;
+        this.severity = severityInput;
     }
     
     
-    
+    /**
+     * get Message.
+     * @return message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * get Category.
+     * @return category
+     */
     public int getCategory() {
         return category;
     }
 
+    /**
+     * get severity.
+     * @return severity
+     */
     public int getSeverity() {
         return severity;
     }
--- src/main/java/game/Logger.java
+++ src/main/java/game/Logger.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,6 +19,12 @@ public class Logger {
     private static java.util.Date date= new java.util.Date();
     private static SimpleDateFormat d = new SimpleDateFormat("dd-M-yyyy hh-mm-ss"); 
     
+    /**
+     * The logging method called to create a log object.
+     * @param message
+     * @param category
+     * @param severity
+     */
     public static void log(String message, int category, int severity){
         
         LogObject tempLog = new LogObject(message,category,severity);
@@ -25,27 +32,41 @@ public class Logger {
         appendToFile(tempLog);
     }
 
-    public static void appendToFile(LogObject log){
+    /**
+     * write a log object to a text file.
+     * @param log
+     */
+    public static void appendToFile(LogObject log) {
         PrintWriter writer;
         try {
-            
-           
-            System.out.println(d.format(date));
-            writer = new PrintWriter(new FileWriter("Logs/log "+d.format(date)+".txt",true));
-            writer.println("["+categoryString[log.getCategory()]+"] "+log.getMessage());
+            writer = new PrintWriter(new FileWriter("Logs/log " + d.format(date) + ".txt", true));
+            writer.println("[" + categoryString[log.getCategory()] + "] " + log.getMessage());
             writer.close();
-            
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
             catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            e.printStackTrace();
         }
 
+    }
+    
+    /**
+     * Returns the list of logs filtered on category and severity.
+     * @param category
+     * @param minSeverity
+     */
+    public static LinkedList<LogObject> getFilteredLogs(ArrayList<Integer> category, int minSeverity) {
+        LinkedList<LogObject> filteredList = new LinkedList<LogObject>();    
+        LogObject lo;
+        for (int i = 0; i < logList.size(); i++) {
+            lo = logList.get(i);
+            if (category.contains(lo.getCategory()) && lo.getSeverity() <= minSeverity) {
+                filteredList.add(lo);
+            }
+        }
+        return filteredList;
     }
 }
