51e8c9c8d8425c5b412a9ff479a096c2788dc904
tim <timbuckers@gmail.com>
2015-09-17 14:41:05 +0200

    log screen with scrollview <feature

--- src/main/java/game/LogScreen.java
+++ src/main/java/game/LogScreen.java
@@ -1,84 +1,118 @@
 package game;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
-import java.util.*;
 public class LogScreen extends JFrame {
 
     JButton startButton;
+    JButton button;
     private Graphics2D g2d;
     private Driver driver;
-    
+    private JList list;
+    private Container pane;
 
-    public LogScreen() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
-
-        
-        setTitle("Log screen");
-        setSize(700, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        URL location = LogScreen.class.getProtectionDomain().getCodeSource().getLocation();
-        
-        setVisible(true);
-
-        setLayout(null);
-        setResizable(false);
-      
-        setSize(699, 699);
-        setSize(700, 700);
-        createButton();
+    public LogScreen() throws UnsupportedAudioFileException, IOException,
+            LineUnavailableException {
 
         driver = new Driver();
-        
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent startGame) {
-                driver.repaint();
-                try {
-                    LogScreen.main(null);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                System.out.println("repaint");
-                
-            }
-        });
-        
-        add(startButton);
+        String title = "Log Screen";
+
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Logging Screen");
+        frame.setSize(200, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        Font boldFont = new Font("Serif", Font.BOLD, 18);
+        Font basicFont = new Font("Serif", Font.PLAIN, 16);
+
+        JLabel textLog = new JLabel("Log");
+        textLog.setFont(boldFont);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        JLabel textCategory = new JLabel("Category");
+        textCategory.setFont(boldFont);
+        rightPanel.add(textCategory);
+
+        String[] cat = Logger.getCategory();
+        for (int i = 0; i < cat.length; i++) {
+            JLabel textCat = new JLabel(cat[i]);
+            textCat.setFont(basicFont);
+            rightPanel.add(textCat);
+        }
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel mainInnerPanel = makeMainInnerPanel();
+
+        JScrollPane scrollPane = new JScrollPane(mainInnerPanel);
+        mainPanel.add(scrollPane);
+
+        // mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        frame.add(textLog, BorderLayout.PAGE_START);
+        frame.add(new JButton("Button 4"), BorderLayout.CENTER);
+        frame.add(mainPanel, BorderLayout.LINE_END);
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
+        /*
+         * startButton.addActionListener(new ActionListener() { public void
+         * actionPerformed(ActionEvent startGame) { driver.repaint(); } });
+         * frame.add(startButton); pane.add(button, BorderLayout.PAGE_START);
+         */
 
     }
-    /**
-     * Create button method.
-     */
-    public void createButton() {
-        startButton = new JButton("Start log");
-        startButton.setBackground(Color.RED);
-        startButton.setForeground(Color.WHITE);
-        startButton.setFont(new Font("Calibri", Font.BOLD, 30));
-        startButton.setOpaque(true);
-        startButton.setBounds(55, 300, 250, 60);
+
+    private JPanel makeHorizontalPanel(String... labelValues) {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        for (String s : labelValues) {
+            JLabel label = new JLabel(s);
+            panel.add(label);
+        }
+        return panel;
     }
+
+    private JPanel makeMainInnerPanel() {
+
+        Font basicFont = new Font("Serif", Font.PLAIN, 16);
+        ArrayList<Integer> category = new ArrayList<Integer>();
+        category.add(1);
+        category.add(2);
+        category.add(3);
+        LinkedList<LogObject> ll = Logger.getFilteredLogs(category, 5);
+        int size = ll.size();
+        ArrayList<JPanel> jPanels = new ArrayList<JPanel>();
+        for (int i = 0; i < size; i++) {
+            jPanels.add(makeHorizontalPanel(ll.pop().getMessage()));
+        }
+        JPanel mainInnerPanel = new JPanel();
+        mainInnerPanel.setLayout(new BoxLayout(mainInnerPanel, BoxLayout.Y_AXIS));
+        for (int i = 0; i < jPanels.size(); i++) {
+            mainInnerPanel.add(jPanels.get(i));
+        }
+        return mainInnerPanel;
+    }
+
     @Override
     public void paint(Graphics graph) {
         try {
@@ -90,7 +124,7 @@ public class LogScreen extends JFrame {
 
             Color black = new Color(0, 0, 0);
             g2d.setFont(new Font("Calibri", Font.BOLD, 40));
-            g2d.drawString("Log: ", 50,  50);
+            g2d.drawString("Log: ", 50, 50);
             g2d.setColor(black);
             g2d.drawRect(50, 50, 600, 400);
 
@@ -102,18 +136,11 @@ public class LogScreen extends JFrame {
             g2d.setFont(new Font("Calibri", Font.PLAIN, 14));
             int size = ll.size();
             for (int i = 0; i < size; i++) {
-            g2d.drawString(ll.pop().getMessage(), 65, 80 + 17 * i);
+                g2d.drawString(ll.pop().getMessage(), 65, 80 + 17 * i);
             }
-            
+
         } catch (IndexOutOfBoundsException e) {
-            
-        }
-    }
-    
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("main called");
-        while (true) {
-        Thread.sleep(1000);
+
         }
     }
 }
--- src/main/java/game/Logger.java
+++ src/main/java/game/Logger.java
@@ -69,4 +69,9 @@ public class Logger {
         }
         return filteredList;
     }
+    
+
+    public static String[] getCategory(){
+        return categoryString;
+    }
 }
