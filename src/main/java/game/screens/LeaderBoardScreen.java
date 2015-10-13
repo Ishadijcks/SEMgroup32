2fdb50692024e8dcda504ccfc095c09395961edb
tim <timbuckers@gmail.com>
2015-10-13 11:19:26 +0200

    Leaderboard screen better structure <organisation

--- src/main/java/game/screens/LeaderBoardScreen.java
+++ src/main/java/game/screens/LeaderBoardScreen.java
@@ -28,19 +28,22 @@ public class LeaderBoardScreen extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
-        
         this.leaderBoard = leaderBoard;
-        
+        addScoreList();
+    }
+    
+    /**
+     * Add the score list as labels to the leaderboard.
+     */
+    public void addScoreList(){
         Font font = new Font("Calibri", Font.PLAIN, 30);
-        
         for (int i = 0; i < leaderBoard.getScoreList().size(); i++) {
         allLabels[i] = new JLabel((i + 1) + ". " + leaderBoard.getScoreList().get(i).toString());
         allLabels[i].setFont(font);
         allLabels[i].setForeground(Color.BLACK);
         allLabels[i].setBounds(20, i * 42, 420, 35);
         add(allLabels[i]);
-    }
-        
+        }
     }
 
 }
