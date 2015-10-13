872ef9b49103fd0b0f0279308fca9f0f830c7ff0
Boning Gong <bgong@student.tudelft.nl>
2015-10-13 13:00:36 +0200

    Refactored part of Painter <organisation

--- src/main/java/game/screens/Painter.java
+++ src/main/java/game/screens/Painter.java
@@ -61,6 +61,7 @@ public class Painter{
     }
     public void paint(Graphics2D g2d) {
         try {
+            this.g2d = g2d;
 
             imageLocation = imageLocation.replace("%20", " ");
             imageLocation = imageLocation.replace("target/classes/", "/");
@@ -70,197 +71,19 @@ public class Painter{
             centerConstant = Settings.getLeftMargin();
             topMargin = Settings.getTopMargin();
 
-            // Draw the game board
             Level curLevel = game.getCurrentLevel();
-            g2d.drawRect(centerConstant, 50, curLevel.getWidth(),
-                    curLevel.getHeight());
-            if (canDrawGame) {
-                g2d.setColor(bg);
-                g2d.fillRect(centerConstant + 1, 51, curLevel.getWidth() - 1,
-                        curLevel.getHeight() - 1);
-                g2d.setColor(Color.BLACK);
-            }
+            
+            // Draw the board for each level
+            drawLevel();
 
             // Draw all the bubbles with a black border
-
-            for (int i = 0; i < curLevel.getBubbleList().size(); i++) {
-                Bubble bubble = curLevel.getBubbleList().get(i);
-                g2d.setColor(bubble.getColor());
-                g2d.fillOval(bubble.getX(), bubble.getY(),
-                        bubble.getDiameter(), bubble.getDiameter());
-                g2d.setColor(Color.black);
-
-                g2d.drawOval(bubble.getX() - 1, bubble.getY() - 1,
-                        bubble.getDiameter() + 2, bubble.getDiameter() + 2);
-            }
+            drawBubbles();
 
             // Draw the walls
-            for (int i = 0; i < curLevel.getWallList().size(); i++) {
-                Wall wall = curLevel.getWallList().get(i);
-                if (wall.isActive()) {
-                    g2d.setColor(wall.getColor());
-                    g2d.fillRect(wall.getxCoord(),
-                            wall.getyCoord() + Settings.getTopMargin(),
-                            wall.getWidth(), wall.getHeight());
-                    g2d.setColor(Color.BLACK);
-                }
-            }
+            drawWalls();
 
             // Draw the ropes
-            if (curLevel.hasRope() && !(iceRope)) {
-                shootRope = true;
-                ropeDurationCounter--;
-
-                Random rand = new Random();
-                int rInt127to153 = rand.nextInt((153 - 127) + 1) + 127;
-                int rInt51to102 = rand.nextInt((102 - 51) + 1) + 51;
-                int rInt69to240 = rand.nextInt((240 - 69) + 1) + 69;
-                int rInt255to8 = rand.nextInt((255 - 8) + 1) + 8;
-                int rInt6to11 = rand.nextInt((11 - 6) + 1) + 6;
-
-                Color fire1 = new Color(255, rInt127to153, 0);
-                Color fire2 = new Color(255, rInt51to102, 0);
-                Color fire3 = new Color(255, rInt69to240, 0);
-                Color fire4 = new Color(250, rInt255to8, rInt6to11);
-
-                Stroke stroke1 = new BasicStroke(1f);
-                Stroke stroke2 = new BasicStroke(3f);
-                Stroke stroke3 = new BasicStroke(5f);
-                Stroke stroke4 = new BasicStroke(7f);
-
-                Color[] colors = { fire4, fire3, fire2, fire1 };
-                Stroke[] strokes = { stroke4, stroke3, stroke2, stroke1 };
-
-                g2d.setColor(fire4);
-                g2d.setStroke(stroke4);
-
-                if (dragonIsRight) {
-                    if (!(addOnce)) {
-                        curLevel.getRope().addX(-6);
-                        addOnce = true;
-                    }
-                    for (int i = 0; i < 4; i++) {
-                        g2d.setColor(colors[i]);
-                        g2d.setStroke(strokes[i]);
-
-                        int random = rand.nextInt((4 - 0) + 1) + 0;
-                        if ((i == 0) && random == 2) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        } else if (i == 1 || i == 2 || i == 3) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        }
-
-                    }
-
-                } else {
-                    if (!(addOnce)) {
-                        curLevel.getRope().addX(-32);
-                        addOnce = true;
-                    }
-                    for (int i = 0; i < 4; i++) {
-                        g2d.setColor(colors[i]);
-                        g2d.setStroke(strokes[i]);
-
-                        int random = rand.nextInt((4 - 0) + 1) + 0;
-                        if ((i == 0) && random == 2) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        } else if (i == 1 || i == 2 || i == 3) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        }
-                    }
-                }
-
-                // Set g2d back to normal settings
-                g2d.setColor(Color.BLACK);
-                Stroke normalStroke = new BasicStroke(1f);
-                g2d.setStroke(normalStroke);
-
-            }
-
-            if (curLevel.hasRope() && iceRope) {
-                shootRope = true;
-                ropeDurationCounter--;
-
-                Random rand = new Random();
-                int rInt219to255 = rand.nextInt((255 - 219) + 1) + 219;
-                int rInt120to102 = rand.nextInt((120 - 102) + 1) + 102;
-                int rInt70to133 = rand.nextInt((133 - 70) + 1) + 70;
-                int rInt6to33 = rand.nextInt((33 - 6) + 1) + 6;
-
-                Color fire1 = new Color(0, rInt219to255, 255);
-                Color fire2 = new Color(36, rInt120to102, 165);
-                Color fire3 = new Color(29, rInt70to133, 209);
-                Color fire4 = new Color(14, rInt6to33, 120);
-
-                Stroke stroke1 = new BasicStroke(1f);
-                Stroke stroke2 = new BasicStroke(3f);
-                Stroke stroke3 = new BasicStroke(5f);
-                Stroke stroke4 = new BasicStroke(7f);
-
-                Color[] colors = { fire4, fire3, fire2, fire1 };
-                Stroke[] strokes = { stroke4, stroke3, stroke2, stroke1 };
-
-                g2d.setColor(fire4);
-                g2d.setStroke(stroke4);
-
-                if (dragonIsRight) {
-                    if (!(addOnce)) {
-                        curLevel.getRope().addX(-6);
-                        addOnce = true;
-                    }
-                    for (int i = 0; i < 4; i++) {
-                        g2d.setColor(colors[i]);
-                        g2d.setStroke(strokes[i]);
-
-                        int random = rand.nextInt((4 - 0) + 1) + 0;
-                        if ((i == 0) && random == 2) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 45);
-                        } else if (i == 1 || i == 2 || i == 3) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 45);
-                        }
-
-                    }
-
-                } else {
-                    if (!(addOnce)) {
-                        curLevel.getRope().addX(-32);
-                        addOnce = true;
-                    }
-                    for (int i = 0; i < 4; i++) {
-                        g2d.setColor(colors[i]);
-                        g2d.setStroke(strokes[i]);
-
-                        int random = rand.nextInt((4 - 0) + 1) + 0;
-                        if ((i == 0) && random == 2) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        } else if (i == 1 || i == 2 || i == 3) {
-                            g2d.drawLine(curLevel.getRope().getX(), curLevel
-                                    .getRope().getY(), curLevel.getRope()
-                                    .getX(), curLevel.getHeight() + 43);
-                        }
-                    }
-                }
-
-                // Set g2d back to normal settings
-                g2d.setColor(Color.BLACK);
-                Stroke normalStroke = new BasicStroke(1f);
-                g2d.setStroke(normalStroke);
-
-            }
+            drawRopes();
 
             // Draw all the players
             for (int i = 0; i < game.getPlayerList().size(); i++) {
@@ -460,5 +283,226 @@ public class Painter{
         }
 
     }
+    
+    /**
+     * Draw the bubbles.
+     */
+    public void drawBubbles() {
+        for (int i = 0; i < game.getCurrentLevel().getBubbleList().size(); i++) {
+            Bubble bubble = game.getCurrentLevel().getBubbleList().get(i);
+            g2d.setColor(bubble.getColor());
+            g2d.fillOval(bubble.getX(), bubble.getY(),
+                    bubble.getDiameter(), bubble.getDiameter());
+            g2d.setColor(Color.black);
+
+            g2d.drawOval(bubble.getX() - 1, bubble.getY() - 1,
+                    bubble.getDiameter() + 2, bubble.getDiameter() + 2);
+        }
+    }
+    
+    /**
+     * Draw the walls.
+     */
+    public void drawWalls() {
+        for (int i = 0; i < game.getCurrentLevel().getWallList().size(); i++) {
+            Wall wall = game.getCurrentLevel().getWallList().get(i);
+            if (wall.isActive()) {
+                g2d.setColor(wall.getColor());
+                g2d.fillRect(wall.getxCoord(),
+                        wall.getyCoord() + Settings.getTopMargin(),
+                        wall.getWidth(), wall.getHeight());
+                g2d.setColor(Color.BLACK);
+            }
+        }
+    }
+    
+    /**
+     * Draw the board for all levels.
+     */
+    public void drawLevel() {
+        g2d.drawRect(centerConstant, 50, game.getCurrentLevel().getWidth(),
+                game.getCurrentLevel().getHeight());
+        if (canDrawGame) {
+            g2d.setColor(bg);
+            g2d.fillRect(centerConstant + 1, 51, game.getCurrentLevel().getWidth() - 1,
+                    game.getCurrentLevel().getHeight() - 1);
+            g2d.setColor(Color.BLACK);
+        }
+    
+    }
+    
+    /**
+     * Draw all the ropes.
+     */
+    public void drawRopes() {
+        if (game.getCurrentLevel().hasRope() && !(iceRope)) {
+            drawNormalRope();
+        }
+
+        if (game.getCurrentLevel().hasRope() && iceRope) {
+            drawIceRope();
+        }
+    }
+    
+    /**
+     * Draw a normal rope.
+     */
+    public void drawNormalRope() {
+        shootRope = true;
+        ropeDurationCounter--;
+
+        Random rand = new Random();
+        int rInt127to153 = rand.nextInt((153 - 127) + 1) + 127;
+        int rInt51to102 = rand.nextInt((102 - 51) + 1) + 51;
+        int rInt69to240 = rand.nextInt((240 - 69) + 1) + 69;
+        int rInt255to8 = rand.nextInt((255 - 8) + 1) + 8;
+        int rInt6to11 = rand.nextInt((11 - 6) + 1) + 6;
+
+        Color fire1 = new Color(255, rInt127to153, 0);
+        Color fire2 = new Color(255, rInt51to102, 0);
+        Color fire3 = new Color(255, rInt69to240, 0);
+        Color fire4 = new Color(250, rInt255to8, rInt6to11);
+
+        Stroke stroke1 = new BasicStroke(1f);
+        Stroke stroke2 = new BasicStroke(3f);
+        Stroke stroke3 = new BasicStroke(5f);
+        Stroke stroke4 = new BasicStroke(7f);
+
+        Color[] colors = { fire4, fire3, fire2, fire1 };
+        Stroke[] strokes = { stroke4, stroke3, stroke2, stroke1 };
+
+        g2d.setColor(fire4);
+        g2d.setStroke(stroke4);
+
+        if (dragonIsRight) {
+            if (!(addOnce)) {
+                game.getCurrentLevel().getRope().addX(-6);
+                addOnce = true;
+            }
+            for (int i = 0; i < 4; i++) {
+                g2d.setColor(colors[i]);
+                g2d.setStroke(strokes[i]);
+
+                int random = rand.nextInt((4 - 0) + 1) + 0;
+                if ((i == 0) && random == 2) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                } else if (i == 1 || i == 2 || i == 3) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                }
+
+            }
+
+        } else {
+            if (!(addOnce)) {
+                game.getCurrentLevel().getRope().addX(-32);
+                addOnce = true;
+            }
+            for (int i = 0; i < 4; i++) {
+                g2d.setColor(colors[i]);
+                g2d.setStroke(strokes[i]);
+
+                int random = rand.nextInt((4 - 0) + 1) + 0;
+                if ((i == 0) && random == 2) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                } else if (i == 1 || i == 2 || i == 3) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                }
+            }
+        }
+
+        // Set g2d back to normal settings
+        g2d.setColor(Color.BLACK);
+        Stroke normalStroke = new BasicStroke(1f);
+        g2d.setStroke(normalStroke);
+    }
+    
+    /**
+     * Draw an ice rope.
+     */
+    public void drawIceRope() {
+        shootRope = true;
+        ropeDurationCounter--;
+
+        Random rand = new Random();
+        int rInt219to255 = rand.nextInt((255 - 219) + 1) + 219;
+        int rInt120to102 = rand.nextInt((120 - 102) + 1) + 102;
+        int rInt70to133 = rand.nextInt((133 - 70) + 1) + 70;
+        int rInt6to33 = rand.nextInt((33 - 6) + 1) + 6;
+
+        Color fire1 = new Color(0, rInt219to255, 255);
+        Color fire2 = new Color(36, rInt120to102, 165);
+        Color fire3 = new Color(29, rInt70to133, 209);
+        Color fire4 = new Color(14, rInt6to33, 120);
+
+        Stroke stroke1 = new BasicStroke(1f);
+        Stroke stroke2 = new BasicStroke(3f);
+        Stroke stroke3 = new BasicStroke(5f);
+        Stroke stroke4 = new BasicStroke(7f);
+
+        Color[] colors = { fire4, fire3, fire2, fire1 };
+        Stroke[] strokes = { stroke4, stroke3, stroke2, stroke1 };
+
+        g2d.setColor(fire4);
+        g2d.setStroke(stroke4);
+
+        if (dragonIsRight) {
+            if (!(addOnce)) {
+                game.getCurrentLevel().getRope().addX(-6);
+                addOnce = true;
+            }
+            for (int i = 0; i < 4; i++) {
+                g2d.setColor(colors[i]);
+                g2d.setStroke(strokes[i]);
+
+                int random = rand.nextInt((4 - 0) + 1) + 0;
+                if ((i == 0) && random == 2) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 45);
+                } else if (i == 1 || i == 2 || i == 3) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 45);
+                }
+
+            }
+
+        } else {
+            if (!(addOnce)) {
+                game.getCurrentLevel().getRope().addX(-32);
+                addOnce = true;
+            }
+            for (int i = 0; i < 4; i++) {
+                g2d.setColor(colors[i]);
+                g2d.setStroke(strokes[i]);
+
+                int random = rand.nextInt((4 - 0) + 1) + 0;
+                if ((i == 0) && random == 2) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                } else if (i == 1 || i == 2 || i == 3) {
+                    g2d.drawLine(game.getCurrentLevel().getRope().getX(), game.getCurrentLevel()
+                            .getRope().getY(), game.getCurrentLevel().getRope()
+                            .getX(), game.getCurrentLevel().getHeight() + 43);
+                }
+            }
+        }
+
+        // Set g2d back to normal settings
+        g2d.setColor(Color.BLACK);
+        Stroke normalStroke = new BasicStroke(1f);
+        g2d.setStroke(normalStroke);
+    }
+    
+    
 
 }
